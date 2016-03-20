2c1c89c6802b03ea6c2f6c646b05f43a82602454
rmellema <rene.mellema@gmail.com>
2016-03-20 12:04:50 +0100

    Added a formula for public announcements

--- code/src/FormulaTester.java
+++ code/src/FormulaTester.java
@@ -14,10 +14,10 @@ public class FormulaTester {
     public static void print(String name, Formula test) {
         String value = test.toString();
         String holds = Boolean.toString(test.evaluate(km));
-        while (value.length() > 50) {
-            for (int i = 49; i >= 0; i--) {
+        while (value.length() > 40) {
+            for (int i = 39; i >= 0; i--) {
                 if (value.charAt(i) == ' ') {
-                    System.out.format("%-20s [ %-50s] %5s\n", name,
+                    System.out.format("%-30s # %-40s# %5s\n", name,
                             value.substring(0, i), holds);
                     value = value.substring(i);
                     name = "";
@@ -26,7 +26,7 @@ public class FormulaTester {
                 }
             }
         }
-        System.out.format("%-20s [ %-50s] %5s\n", name, value, holds);
+        System.out.format("%-30s # %-40s# %5s\n", name, value, holds);
     }
 
     public static void init(int[][] array) {
@@ -59,7 +59,8 @@ public class FormulaTester {
         print("1 maybes p01", new Maybe(1, p01));
         System.out.println(new String(new char[80]).replace('\0', '='));
         Card c00 = new Card(0, 0);
-        Or cards = new Or(p00, new PropVar(c00, 1), new PropVar(c00, 2), new PropVar(c00, 3), new PropVar(c00, 4));
+        Or cards = new Or(p00, new PropVar(c00, 1), new PropVar(c00, 2),
+                new PropVar(c00, 3), new PropVar(c00, 4));
         print("Some has card 0, 0", cards);
         print("It is known", new EveryKnows(set(1, 2, 3, 4), cards));
         print("You know nothing", new EveryKnows(set(1, 2, 3, 4), p00));
@@ -67,6 +68,12 @@ public class FormulaTester {
         print("Doesn\'t know all", new ImplicitKnow(set(1, 2, 3, 4), p11));
         // Uncomment, run, and go watch a movie. It takes about that long
         //print("Some has card 0, 0", new CommonKnow(set(1,2,3,4), cards));
+        print("The envelope is secret", new CommonKnow(set(1, 2, 3, 4), p00));
+        System.out.println(new String(new char[80]).replace('\0', '='));
+        //print("Public", new PublicAnnouncement(p00,
+        //        new CommonKnow(set(1, 2, 3, 4), p00)));
+        print("Untrue announcement", new PublicAnnouncement(p01, new And(p11, p11.negate())));
+        print("True announcement", new PublicAnnouncement(p00, new And(p11, p11.negate())));
 
     	//TODO Test the relation query functions
     }
---
+++ code/src/PublicAnnouncement.java
@@ -0,0 +1,45 @@
+/**
+ * Created by rene on 20/03/16.
+ */
+public class PublicAnnouncement extends Formula {
+    private Formula announcement;
+    private Formula formula;
+
+    /**
+     * Create a new public announcement
+     * @param announcement The announcement that is made
+     * @param formula The formula that should hold afterwards
+     */
+    public PublicAnnouncement(Formula announcement, Formula formula) {
+        this.announcement = announcement;
+        this.formula = formula;
+    }
+
+    /**
+     * Evaluate a Formula in a state using the given model
+     *
+     * @param model The model used for evaluation
+     * @param state The state to evaluate the Formula in
+     * @return `true` if the Formula evaluate in state `state`, `false` otherwise
+     */
+    @Override
+    public boolean evaluate(KripkeModel model, int state) {
+        KripkeModel copy = new KripkeModel(model);
+        copy.publicAnnouncement(this.announcement);
+        if (copy.hasState(state))
+            return this.formula.evaluate(copy, state);
+        else
+            return true; //State is gone, so true by default
+    }
+
+    /**
+     * Return a string representation of this Formula
+     *
+     * @return String representation
+     */
+    @Override
+    public String toString() {
+        return "[" + this.announcement.toString() + "] " +
+                this.formula.toString();
+    }
+}
