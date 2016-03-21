b9fb04808681003d1109d823ab6772098baafe6e
rmellema <rene.mellema@gmail.com>
2016-03-21 23:57:44 +0100

    Added the CardTest class

---
+++ code/src/CardTest.java
@@ -0,0 +1,22 @@
+/**
+ * Created by rene on 21/03/16.
+ */
+public class CardTest {
+    public static void main(String[] args) {
+        Card c1 = new Card(1, 1);
+        Card c2 = new Card(1, 1);
+        Card c3 = new Card(2, 1);
+        Card c4 = new Card(1, 2);
+        System.out.println(c1.equals(c2));
+        System.out.println(c1.equals(c3));
+        System.out.println(c1.equals(c4));
+        System.out.println(c2.equals(c3));
+        System.out.println(c2.equals(c4));
+        System.out.println(c3.equals(c4));
+        CardSet set = new CardSet(c1, c3);
+        System.out.println(set.contains(c1));
+        System.out.println(set.contains(c2));
+        System.out.println(set.contains(c4));
+
+    }
+}
