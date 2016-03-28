c92c887ed020490d59e208fd47501ea482b061ab
Laura van de Braak <lauravdbraak@gmail.com>
2016-03-28 17:07:31 +0200

    added 'risky' accusation strategy

--- code/src/AccusationStrategy.java
+++ code/src/AccusationStrategy.java
@@ -1,3 +1,6 @@
+
+import java.util.Random;
+
 /**
  * 
  * @author lauravandebraak
@@ -15,14 +18,23 @@ class AccusationStrategy extends Strategy {
     }
     
     public CardSet strategy(KripkeModel model, int agent){
-        if(strategy == "default"){
+        if(strategy.equals("default")){
             return defaultStrat(model, agent);
+        }
+        if(strategy.equals("risk")){
+            return riskStrat(model, agent);
         }
         return null;
     }
     
+    /**
+     * Default Strategy: if an agent knows all cards in the envelope, they will make an accusation
+     * @param model the current model
+     * @param agent the current agent
+     * @return a suspicion if one is made, otherwise null
+     */
     private CardSet defaultStrat(KripkeModel model, int agent){
-        int c = 0, n = 0, found = 0, number = 0;
+        int c, n, found = 0, number;
         int cat = model.point().getCategories();
         Card[] cards = new Card[cat];
     
@@ -39,6 +51,55 @@ class AccusationStrategy extends Strategy {
             }
         }
         if(found == cat){
+            return new CardSet(cards);
+        }
+        
+        return null;
+    }
+    
+    /**
+     * A risky strategy: if an agent holds only two cards as possible, 
+     * and knows all the rest, make a guess so someone can not win before you
+     * @param model the current model
+     * @param agent the current agent
+     * @return a suspicion if one is made, otherwise null
+     */
+    private CardSet riskStrat(KripkeModel model, int agent){
+        int c, n, found = 0, number, notC = -1, not1 = -1, not2 = -1;
+        int cat = model.point().getCategories();
+        Card[] cards = new Card[cat];
+        Random rand = new Random();
+        
+        
+        for (c=0; c < cat; c++){
+            number = model.point().numberOfCards(c);
+            for (n=0; n < number; n++){
+                Card test = new Card(c, n);
+                if((new Know(agent, new PropVar(test, 0))).evaluate(model)) {
+                    System.out.println("Agent knows card " + test);
+                    found++;
+                    cards[c] = test;
+                    break;
+                }
+                if(not1 == -1){
+                    not1 = n;
+                }else{
+                    not2 = n;
+                }
+                if(n == number-1){
+                    notC = c;
+                }
+            }
+        }
+        if(found == cat){
+            return new CardSet(cards);
+        }
+        if(found == cat-1){
+            if(rand.nextBoolean()){
+                cards[notC] = new Card(notC, not1);
+            }else{
+                cards[notC] = new Card(notC, not2);
+            }
             return new CardSet(cards);
         }
         
